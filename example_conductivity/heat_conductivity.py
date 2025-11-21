b5ae60e0fc00ee3c90dd6eaa8edd0ac41f0847d7
APiccolo89 <andreapiccolo89@gmail.com>
2025-11-21 11:54:31 +0000

    New Branch

--- example_conductivity/heat_conductivity.py
+++ example_conductivity/heat_conductivity.py
@@ -9,57 +9,208 @@ elif option_k == 2 or option_k == 3 :
     k  = 2.47 # [Wm-1K-1]
     a = 0.33/1e9 # [Wm-1K-1GPa-1] GPA PD!!!!
     n  = 0.48 
+    
+    Script description: This script computes the density of a material as a function of pressure and temperature
+using a combined equation of state approach based on the Birch-Murnaghan equation of state
+and thermal expansivity models from Grose and Alfonso (2013). 
+The idea is to understand how to compute these properties and giving a general framework for future implementation and starting 
+a work flow that begins with the thermal structure of the oceanic lithosphere. 
+
+I need a suitable material property set for the oceanic lithosphere, e.g., peridotite or basalt.
+Then compute the density profile as a function of pressure and temperature. 
 '''
+from scipy.optimize import fsolve 
+def compute_diffusivity_T(pdb, T):
+    """Compute thermal diffusivity based on empirical formula. Ora, sti stronzi devono smetterla di rilasciare le porco dio di
+    costanti usando le loro SI di sta merda: USATE M S KG standard, perche' in ogni fottuta equazione termodinamica si usano quelle
+    ma che cazzo di merda hanno nel cervello?  Non solo, le equazioni sono sbagliate, dio maiale infame, Korenaga 2016 da una versione corretta
+    a quanto pare era T/c no T*c, ma chissene se devo perdere il mio tempo, tanto sono immortale, vero? Che si fottano, poi appena vedono un mio articolo
+    fanno la punta al cazzo per ragioni del cazzo. The simplest bug of all the time, forgot a parenthesis.
+    """
+    # Data from Fosterite
+    a = 0.565/1e6  
+    b = 0.67/1e6
+    c = 590.0
+    d = 1.4/1e6
+    e = 135.0
+    T = T-273.15
+    D = a + b * np.exp(-T/c) + d * np.exp(-T/e)
+    
+    return D  # in mm^2/s
 
-k0 = 4.10 
-a = 0.032/1e9
-n = 0.493
+def compute_radiative_conductivity_T(T,d):
+    """Compute radiative conductivity based on empirical formula.
+    """
+    def A(d):
+        return 1.8 * (1 - np.exp(-d**1.3 / 0.15)) - (1 - np.exp(-d**0.5 / 5.0))
 
-k1  = 2.47 # [Wm-1K-1]
-a1 = 0.033/1e9 # [Wm-1K-1GPa-1] GPA PD!!!!
-n1  = 0.48 
+    def B(d):
+        return 11.7 * np.exp(-d / 0.159) + 6.0 * np.exp(-d**3 / 10.0)
+
+    def T_A(d):
+        return 490.0 + 1850.0 * np.exp(-d**0.315 / 0.825) + 875.0 * np.exp(-d / 0.18)
+
+    def T_B(d):
+        return 2700.0 + 9000.0 * np.exp(-d**0.5 / 0.205)
+
+    def x_A(d):
+        return 167.5 + 505.0 * np.exp(-d**0.5 / 0.85)
+
+    def x_B(d):
+        return 465.0 + 1700.0 * np.exp(-d**0.94 / 0.175) 
+
+     # density in g/cm3 for Fosterite      
+
+    cfA =A(d) * np.exp(-(T-T_A(d))**2/ (2*x_A(d) ** 2 ))
+    cfB = B(d) * np.exp(-(T - T_B(d))**2 / (2* x_B(d)**2))
+    krad = A(d) * np.exp(-(T-T_A(d))**2/ (2*x_A(d) ** 2 )) + B(d) * np.exp(-(T - T_B(d))**2 / (2* x_B(d)**2))
+
+    return krad # in W/mK
+
+def compute_diffusivity_P(P):
+    a = 0.05/1e9 
+    
+    D = np.exp(a * P)
+
+    return D   # it is the dimensionless, i hate these fuckers 
 
 
-alpha = 1e-5  # thermal expansivity 1/K
-T_ref = 273.15 + 25.0  # K
-beta = 2e-12  # compressibility 1/Pa
-Cp = 1000  # J/kgK
+def compute_thermal_capacity_T(T):
+    """_summary_
+    Altro Rant: troppo difficile fare le cose con le cazzo di SI jesus christ, ma che cazzo di merda hanno nel cervello questi stronzi, poi si lamentano che gli articoli non vengono accettati
+    1) Kj = 1000 J
+    Args:
+        T (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
+    
+    a = 1.6108   * 1e3
+    b = -12.4788 * 1e3
+    c = 1728477  * 1e3
+    Cp = (a + b * T ** (-0.5) + c * T ** (-3))
+    
+    return Cp
+    
+
+def find_root_V0(P, K0, K0p):
+    
+    def f(V_V0):
+        return (3.0/2.0)*K0*((V_V0)**(7.0/3.0)-(V_V0)**(5.0/3.0))*(1.0 + (3.0/4.0)*(K0p -4.0)*((V_V0)**(2.0/3.0)-1.0)) - P
+    
+    V_V0_initial_guess = 1.0 - P/(3.0*K0)  # Initial guess based on small compression
+    
+    V_V0_solution = fsolve(f, V_V0_initial_guess)[0]
+    
+    return V_V0_solution
+
+# Grose and Alfonso 
+def compute_isothermal_volume_change(P, K0, K0p): 
+    
+    a = 1.0 
+    
+    a = find_root_V0(P, K0, K0p)
+    V_V0 = a    
+    return V_V0
+
+def compute_thermal_expansivity_pressure_dependent(V_V0,Gr0):
+    
+    alphaP = V_V0 * np.exp((Gr0+1.0)*(V_V0**(-1) - 1.0)) 
+    
+    return alphaP
+
+def compute_thermal_expansitivity_temperature_dependent(T, alpha0, alpha1 , T0 , q):
+    
+    alphaT = alpha0 * (T - T0)  + 0.5 * alpha1 * (T**2 - T0**2) 
+
+    return alphaT
+    
+
+T = np.linspace(298.15, 1600+273.15, 1001) #[K]
+
+k_rad = np.zeros(len(T))
+
+k_rad = compute_radiative_conductivity_T(T,0.5)
+
+P = np.linspace(0, 12e9, 600) #[Pa]
+
+K0 = 130e9 #[Pa]
+
+K0p = 4.8  
+
+V_V0 = np.zeros(len(P))
+
+alpha_p = np.zeros(len(P))
+
+for i in range(len(P)):
+
+    V_V0[i] = compute_isothermal_volume_change(P[i], K0, K0p)
+    
+    alpha_p[i] = compute_thermal_expansivity_pressure_dependent(V_V0[i], Gr0=6.0)
 
 
+alpha_T = compute_thermal_expansitivity_temperature_dependent(T, alpha0=2.832e-5,alpha1 = 0.758e-8 , T0=273.15, q=1.0)
+
+rho_0 = 3330.0 #[kg/m3]
+
+rho    = np.zeros((len(P), len(T)))
+rho2   = np.zeros((len(P), len(T)))
+Kb     = (2*100e9*(1+0.25))/(3*(1-0.25*2))
+alpha0 =  2.832e-5
+alpha1 =  3.79e-8 #[1/K2]
+k      = np.zeros((len(P), len(T)))
+k_rad  = np.zeros((len(P), len(T))) 
+Cp  = np.zeros((len(P), len(T))) 
+kp = np.zeros((len(P), len(T)))
 
 
-T0 = 298.0
-P = np.linspace(0,35e3*3300*9.81,1000)
-T = np.linspace(273.15,1100, 1000) # Pa
-PP,TT = np.meshgrid(P,T)
-rho = 2700*np.exp(-alpha*(TT - 273.15))*np.exp(beta*PP)  # kg/m3
+for i in range(len(P)):
+    for j in range(len(T)):
+        
+        rho[i,j]   = rho_0 * V_V0[i] * (1-alpha_p[i] * alpha_T[j])
+        rho2[i,j]  = rho_0 * np.exp(P[i]/Kb) * (1 - (alpha0 * (T[j]-273.15) + 0.5 * alpha1 * (T[j]**2 - 273.15**2)))
+        k[i,j]     = compute_diffusivity_T(P[i], T[j]) * rho[i,j] * compute_thermal_capacity_T(T[j]) * compute_diffusivity_P(P[i])
+        k_rad[i,j] = compute_radiative_conductivity_T(T[j],0.5)
+        Cp[i,j]    = compute_thermal_capacity_T(T[j])
+        kp[i,j]    = compute_diffusivity_P(P[i])
+        
 
-K1 = k0 * (1 + a*PP)*(T0/TT)**n
-K2 = k1 * (1 + a1*PP)*(T0/TT)**n1
-tk = 5e3 
-td1 = tk**2/(K1/(rho*Cp))/365.25/24/60/60/1e6  # in Myr
-td2 = tk**2/(K2/(rho*Cp))/365.25/24/60/60/1e6  # in M
 
-fig, ax = plt.subplots(1,3, figsize=(15,5))
-cs1 = ax[0].contourf(PP/1e9, TT-273.15, K1, levels=50, cmap='viridis')
-fig.colorbar(cs1, ax=ax[0])
-ax[0].set_title('Conductivity model 1')
-ax[0].set_xlabel('Pressure (GPa)')
-ax[0].set_ylabel('Temperature (C)')
-cs2 = ax[1].contourf(PP/1e9, TT-273.15, K2, levels=50, cmap='viridis')
-fig.colorbar(cs2, ax=ax[1])
-ax[1].set_title('Conductivity model 2')
-ax[1].set_xlabel('Pressure (GPa)')
-ax[1].set_ylabel('Temperature (C)')
-cs3 = ax[2].contourf(PP/1e9, TT-273.15, td1 - td2, levels=50, cmap='bwr')
-fig.colorbar(cs3, ax=ax[2])
-ax[2].set_title('Difference in thermal diffusion time (Myr)')
-ax[2].set_xlabel('Pressure (GPa)')
-ax[2].set_ylabel('Temperature (C)')
-plt.tight_layout()
+import cmcrameri as cm
+plt.figure(figsize=(8,6))
+ax = plt.gca()
+a0 = ax.contourf(T-273.15,-P/1e9, (k+k_rad), levels=10, cmap='cmc.lipari')
+b0 = ax.contour(T-273.15,-P/1e9, (k+k_rad), levels=np.linspace(1,10,num=10), colors='w', linewidths=0.5)
+ax.clabel(b0, inline=1, fontsize=10, fmt='%1.1f')
+plt.colorbar(a0,label='K (W/m/K)')
+plt.ylabel('Pressure (GPa)')
+plt.xlabel('Temperature (C)')
+plt.title('Conductivity as a function of Pressure and Temperature')
+plt.show()  
+
+plt.figure(figsize=(8,6))
+plt.contourf(T-273.15,P/1e9, (k_rad), levels=10, cmap='gist_heat')
+plt.colorbar(label='Krad (W/m/K)')
+plt.ylabel('Pressure (GPa)')
+plt.xlabel('Temperature (C)')
+plt.title('Conductivity as a function of Pressure and Temperature')
+plt.show()  
+
+
+plt.figure(figsize=(8,6))
+plt.contourf(T-273.15,P/1e9, (Cp), levels=10, cmap='gist_heat')
+plt.colorbar(label='Cp (J/kg/K)')
+plt.ylabel('Pressure (GPa)')
+plt.xlabel('Temperature (C)')
+plt.title('Conductivity as a function of Pressure and Temperature')
+plt.show()  
+
+plt.figure(figsize=(8,6))
+plt.contourf(T-273.15,P/1e9, (rho), levels=10, cmap='viridis')
+plt.colorbar(label='Density (kg/m3)')
+plt.ylabel('Pressure (GPa)')
+plt.xlabel('Temperature (C)')
+plt.title('Density as a function of Pressure and Temperature')
 plt.show()
-
-Cp = 0.0*T 
-
-Cp[T<846]  = (199.5 * 0.0857 * T[T<846])# - 5.0e6 * T[T<846] **(-2))
-Cp[T>=846] = (229 * 0.0323 * T[T>=846])# - 47.9e6 * T[T>=846] **(-2))
