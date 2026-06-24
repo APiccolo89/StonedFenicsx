[BUG]: I found a bug. I implement the new parametrisation of the shear heating. ->Bug: scaling. I was scaling the Bdisl of the weak zone with the wrong scaling factor. -> Now I fixed but I need to redo all the tests.

stonedfenicsx/solver_module/problems_solution.py
@@ -426,9 +426,8 @@ class Global_thermal(Problem):
                                           ,wz_tk:float
                                           ,sc:Scal):
 
-        reg_strain = fem.Constant(decoupling.function_space.mesh,np.float64(1e-20 /(1/sc.T)))
 
-        e_II_fr = reg_strain + 0.5 * (vs *  1 /wz_tk)  # Second invariant strain rate
+        e_II_fr = 0.5 * (vs *  1 /wz_tk)  # Second invariant strain rate
 
         # -> compute the plastic strain rate
 