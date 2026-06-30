af8a5670b53738ccb306fd14bdd575e9110fde31
APiccolo89 <andreapiccolo89@gmail.com>
2026-06-30 12:22:29 +0100

    [A]: harmonising, and correcting bugs that I found in other. [B]: trying to make it pepi style as well

--- stonedfenicsx/stonedfenicsx.py
+++ stonedfenicsx/stonedfenicsx.py
@@ -2,12 +2,19 @@ from stonedfenicsx.config.input_parser import Input
 from stonedfenicsx.config.phase_db import PhInput
 from stonedfenicsx.config.simulation_config import configure_simulation
 from stonedfenicsx.solver_module.solution_routine import solution_routine
+from stonedfenicsx.utils import timing_function
 
+@timing_function
 def stoned_fenicsx(inp:Input,ph_in:PhInput) -> None:
+    """Main function : it calls the configure simulation with the input 
+    data, then, send the simulation configuration into the main solver
+
+    Args:
+        inp (Input): _description_
+        ph_in (PhInput): _description_
+    """
 
     ctrl_sim, pdb, mesh, sc = configure_simulation(ph_in=ph_in,inp=inp)
-
-
 
     solution_routine(ctrl_sim,pdb,mesh,sc)
 
