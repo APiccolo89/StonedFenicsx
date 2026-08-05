db01028310741c6cc4511761b85697c120bb8d4c
APiccolo89 <andreapiccolo89@gmail.com>
2026-08-05 22:25:00 +0100

    Quick change

--- stonedfenicsx/output.py
+++ stonedfenicsx/output.py
@@ -253,8 +253,9 @@ class OUTPUT():
             self.xdmf_main.write_function(self.e_ii,          time)
             self.xdmf_main.write_function(self.eta,         time)
             self.xdmf_main.write_function(self.flux,         time)
-            self.xdmf_main.write_function(self.shear_heating,time)
             self.xdmf_main.write_function(self.tag,          time)
+            self.xdmf_main.write_function(self.partition,          time)
+
             
             self.close()
         
@@ -277,7 +278,6 @@ class OUTPUT():
                 ufile_xdmf.write_function(self.e_ii)
                 ufile_xdmf.write_function(self.eta)
                 ufile_xdmf.write_function(self.flux)
-                ufile_xdmf.write_function(self.shear_heating)
                 ufile_xdmf.write_function(self.tag)
                 ufile_xdmf.write_function(self.partition)
                 self.domain.mesh.geometry.x[:] = coord
